a64cf304b1dc39ea3af825be23eba59fcbdec845
Nikenzsk6 <ajaniken307@gmail.com>
2025-04-27 16:12:42 +0700

    method2 --> Penambahan method volume kerucut

--- src/t4/method2.java
+++ src/t4/method2.java
@@ -4,6 +4,8 @@
  */
 package t4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HP
@@ -14,10 +16,20 @@ public class method2 {
  double luas = Math.PI * Math.pow(jari2, 2);
  return luas;
  }
+    void VolumeKerucut(double jari2,double tinggi){
+     double volume = (1.0/3.0)*Math.PI * Math.pow(jari2, 2) * tinggi;
+        System.out.println("Volume kerucut = "+Math.round(volume));
+    }
  public static void main(String[] args) {
  method2 mt = new method2();
- System.out.print(mt.luas_lingkaran(20));
- System.exit(0);
+ System.out.println(mt.luas_lingkaran(20));
+
+ Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jari-jari kerucut = ");
+        double jari2 = input.nextDouble();
+        System.out.print("Masukkan tinggi kerucut = ");
+        double tinggi = input.nextDouble();
+    mt.VolumeKerucut(jari2, tinggi);
  }
 }
 
