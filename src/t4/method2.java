ee7fe8a9c2ae702a3cac8f8989bcc2edaddb6c77
Nikenzsk6 <ajaniken307@gmail.com>
2025-04-30 09:28:07 +0700

    method2 --> Penambahan method volume tabung dan bola

--- src/t4/method2.java
+++ src/t4/method2.java
@@ -17,19 +17,37 @@ public class method2 {
  return luas;
  }
     void VolumeKerucut(double jari2,double tinggi){
-     double volume = (1.0/3.0)*Math.PI * Math.pow(jari2, 2) * tinggi;
+     double volume = (1.0/3.0)*3.14* Math.pow(jari2, 2) * tinggi;
         System.out.println("Volume kerucut = "+Math.round(volume));
+    }
+    void volumeTabung(double jr2,double t){
+        double v_tabung = 3.14*Math.pow(jr2, 2)*t;
+        System.out.println("Volume Tabung = "+Math.round(v_tabung));
+    }
+    void Volumebola(double jari_2){
+        double volBola = (0.4/0.3)*3.14 *Math.pow(jari_2, 3);
+        System.out.println("Volume Bola = "+Math.round(volBola));
     }
  public static void main(String[] args) {
  method2 mt = new method2();
  System.out.println(mt.luas_lingkaran(20));
 
  Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan jari-jari kerucut = ");
+        System.out.print("\nMasukkan jari-jari kerucut = ");
         double jari2 = input.nextDouble();
         System.out.print("Masukkan tinggi kerucut = ");
         double tinggi = input.nextDouble();
     mt.VolumeKerucut(jari2, tinggi);
+    
+        System.out.print("\nMasukkan jari-jari tabung = ");
+        double jr2 = input.nextDouble();
+        System.out.print("Masukkan tinggi tabung = ");
+        double t = input.nextDouble();
+    mt.volumeTabung(jr2, t);
+        System.out.print("\nMasukkan jari-jari bola = ");
+        double jari_2 = input.nextDouble();
+    mt.Volumebola(jari_2);
+    System.exit(0);
  }
 }
 
