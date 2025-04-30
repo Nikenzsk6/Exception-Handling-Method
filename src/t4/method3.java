607d3c0b3e1aec0063c9334ca51f9704be9520a6
Nikenzsk6 <ajaniken307@gmail.com>
2025-04-30 09:59:49 +0700

    method3 --> merapikan tampilan

--- src/t4/method3.java
+++ src/t4/method3.java
@@ -11,24 +11,24 @@ import javax.swing.JOptionPane;
 public class method3 {
     void P_Panjang(int p,int l){
         JOptionPane.showMessageDialog(null,"Mencari Luas Persegi Panjang");
-        p = Integer.parseInt(JOptionPane.showInputDialog("Masukkan panjang"));
-        l = Integer.parseInt(JOptionPane.showInputDialog("Masukkan lebar"));
+        p = Integer.parseInt(JOptionPane.showInputDialog("Masukkan panjang (cm)"));
+        l = Integer.parseInt(JOptionPane.showInputDialog("Masukkan lebar (cm)"));
         int Luas_P = p*l;
-        JOptionPane.showMessageDialog(null, "Luas Persegi Panjang "+Luas_P);
+        JOptionPane.showMessageDialog(null, "Luas Persegi Panjang "+Luas_P+"cm.");
         
     }void Segitiga(int a,int t){
         JOptionPane.showMessageDialog(null,"Mencari Luas Segitiga");
-        a = Integer.parseInt(JOptionPane.showInputDialog("Masukkan alas"));
-        t = Integer.parseInt(JOptionPane.showInputDialog("Masukkan tinggi"));
+        a = Integer.parseInt(JOptionPane.showInputDialog("Masukkan alas (cm)"));
+        t = Integer.parseInt(JOptionPane.showInputDialog("Masukkan tinggi (cm)"));
         double  Luas_Sgt = 0.5*a*t;
-        JOptionPane.showMessageDialog(null, "Luas Segitiga "+Math.round(Luas_Sgt));
+        JOptionPane.showMessageDialog(null, "Luas Segitiga "+Math.round(Luas_Sgt)+"cm.");
         
     
-    }void Linkaran(double jari2){
+    }void Lingkaran(double jari2){
         JOptionPane.showMessageDialog(null,"Mencari Luas Linkaran");
-        jari2 = Integer.parseInt(JOptionPane.showInputDialog("Masukkan jari-jari"));
-        double  Luas_Link = 3.14 * Math.pow(jari2, 2);
-        JOptionPane.showMessageDialog(null, "Luas Linkaran "+Luas_Link);
+        jari2 = Integer.parseInt(JOptionPane.showInputDialog("Masukkan jari-jari (cm)"));
+        double  Luas_Ling = 3.14 * Math.pow(jari2, 2);
+        JOptionPane.showMessageDialog(null, "Luas Lingkaran "+Luas_Ling+"cm.");
         
     }
     
@@ -37,7 +37,7 @@ public class method3 {
         
         m3.P_Panjang(0, 0);
         m3.Segitiga(0, 0);
-        m3.Linkaran(0);
+        m3.Lingkaran(0);
         
     }
 }
