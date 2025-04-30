9fcc9613cad6f2431c29c8204f8c83c014e23a33
Nikenzsk6 <ajaniken307@gmail.com>
2025-04-30 09:56:34 +0700

    method3 --> kode method luas linkaran

--- src/t4/method3.java
+++ src/t4/method3.java
@@ -14,16 +14,22 @@ public class method3 {
         p = Integer.parseInt(JOptionPane.showInputDialog("Masukkan panjang"));
         l = Integer.parseInt(JOptionPane.showInputDialog("Masukkan lebar"));
         int Luas_P = p*l;
-        JOptionPane.showMessageDialog(null, "Luas Persegi Panjang"+Luas_P);
+        JOptionPane.showMessageDialog(null, "Luas Persegi Panjang "+Luas_P);
         
     }void Segitiga(int a,int t){
         JOptionPane.showMessageDialog(null,"Mencari Luas Segitiga");
         a = Integer.parseInt(JOptionPane.showInputDialog("Masukkan alas"));
         t = Integer.parseInt(JOptionPane.showInputDialog("Masukkan tinggi"));
         double  Luas_Sgt = 0.5*a*t;
-        JOptionPane.showMessageDialog(null, "Luas Persegi Panjang"+Luas_Sgt);
+        JOptionPane.showMessageDialog(null, "Luas Segitiga "+Math.round(Luas_Sgt));
         
     
+    }void Linkaran(double jari2){
+        JOptionPane.showMessageDialog(null,"Mencari Luas Linkaran");
+        jari2 = Integer.parseInt(JOptionPane.showInputDialog("Masukkan jari-jari"));
+        double  Luas_Link = 3.14 * Math.pow(jari2, 2);
+        JOptionPane.showMessageDialog(null, "Luas Linkaran "+Luas_Link);
+        
     }
     
     public static void main(String[] args) {
@@ -31,7 +37,7 @@ public class method3 {
         
         m3.P_Panjang(0, 0);
         m3.Segitiga(0, 0);
-       
+        m3.Linkaran(0);
         
     }
 }
