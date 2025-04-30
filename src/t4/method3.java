13663995056ad2389cbc6dea39201de52e14add5
Nikenzsk6 <ajaniken307@gmail.com>
2025-04-30 09:51:47 +0700

    method3 --> kode method luas segitiga

--- src/t4/method3.java
+++ src/t4/method3.java
@@ -10,16 +10,28 @@ import javax.swing.JOptionPane;
  */
 public class method3 {
     void P_Panjang(int p,int l){
+        JOptionPane.showMessageDialog(null,"Mencari Luas Persegi Panjang");
         p = Integer.parseInt(JOptionPane.showInputDialog("Masukkan panjang"));
         l = Integer.parseInt(JOptionPane.showInputDialog("Masukkan lebar"));
         int Luas_P = p*l;
         JOptionPane.showMessageDialog(null, "Luas Persegi Panjang"+Luas_P);
         
+    }void Segitiga(int a,int t){
+        JOptionPane.showMessageDialog(null,"Mencari Luas Segitiga");
+        a = Integer.parseInt(JOptionPane.showInputDialog("Masukkan alas"));
+        t = Integer.parseInt(JOptionPane.showInputDialog("Masukkan tinggi"));
+        double  Luas_Sgt = 0.5*a*t;
+        JOptionPane.showMessageDialog(null, "Luas Persegi Panjang"+Luas_Sgt);
+        
+    
     }
+    
     public static void main(String[] args) {
         method3 m3 = new method3();
         
         m3.P_Panjang(0, 0);
+        m3.Segitiga(0, 0);
+       
         
     }
 }
